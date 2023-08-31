a04fa0f9bb13c9ec0ca73f5515a3fed5d1e67869
Andriy Pashynnyk <tobirawork@gmail.com>
2023-08-31 14:24:56 +0300

    cleanup, new favicon

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -1,9 +1,7 @@
 export default function NotFound() {
   return (
-    <main class="text-center mx-auto p-4 uppercase">
-      <h1 class="max-6-xs text-6xl font-thin my-16">
-        404
-      </h1>
+    <main class="text-center mx-auto p-4">
+      <h1 class="uppercase text-6xl my-40">404</h1>
     </main>
   );
 }
