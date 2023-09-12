700d944033ec1c379a00c9cca367c09596963d5e
Andriy Pashynnyk <tobirawork@gmail.com>
2023-09-12 15:17:21 +0300

    add skeleton and suspense for podcast notes list loading

---
+++ src/components/PodcastNoteSkeleton.tsx
@@ -0,0 +1,42 @@
+export function PodcastNoteSkeleton() {
+  return (
+    <div class="animate-pulse my-16 pl-4 border-l-2 border-gray-500 cursor-pointer flex flex-col gap-4">
+      <div class="flex items-center gap-2">
+        <div class="h-2 rounded w-60 bg-gray-400 my-2" />
+      </div>
+
+      <div class="flex items-center">
+        <div class="w-6 h-6 rounded-sm inline-block mr-2 bg-gray-800" />
+
+        <div class="h-1.5 rounded w-36 bg-gray-600" />
+      </div>
+
+      <div class="my-8 flex flex-col gap-2">
+        <div class="flex items-center gap-2">
+          <div class="h-2.5 rounded w-8 bg-gray-200" />
+          <div class="h-2.5 rounded w-44 bg-gray-200" />
+          <div class="h-2.5 rounded w-16 bg-gray-200" />
+        </div>
+
+        <div class="flex items-center gap-2">
+          <div class="h-2.5 rounded w-12 bg-gray-200" />
+          <div class="h-2.5 rounded w-32 bg-gray-200" />
+          <div class="h-2.5 rounded w-24 bg-gray-200" />
+        </div>
+
+        <div class="flex items-center gap-2">
+          <div class="h-2.5 rounded w-32 bg-gray-200" />
+          <div class="h-2.5 rounded w-8 bg-gray-200" />
+          <div class="h-2.5 rounded w-20 bg-gray-200" />
+          <div class="h-2.5 rounded w-6 bg-gray-200" />
+        </div>
+      </div>
+
+      <div class="h-2 self-end rounded text-xs bg-gray-600 w-20 mr-2" />
+    </div>
+  );
+}
+
+function formatPodcastTitle(title: string) {
+  return title.replace(" (🔓 for Andriy Pashynnyk)", ""); // this postfix is added by the private podcasts feeds
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,43 +1,42 @@
 import { getXataClient, PodcastEpisodeNotesRecord } from "~/xata";
-import { useRouteData } from "solid-start";
-import { For } from "solid-js";
+import server$ from "solid-start/server";
+import { createResource, For, Show, createEffect, Suspense } from "solid-js";
 import { PodcastNote } from "~/components/PodcastNote";
-import { createServerData$ } from "solid-start/server";
+import { PodcastNoteSkeleton } from "~/components/PodcastNoteSkeleton";
 
-export function routeData() {
-  return createServerData$(
-    async () => {
-      const xata = getXataClient();
+const fetchPodcastNotes = server$(async () => {
+  const xata = getXataClient();
 
-      const podcastNotes = (await xata.db.PodcastEpisodeNotes.sort(
-        "createdAt",
-        "desc",
-      )
-        .select(["*", "podcastEpisode.*", "podcastEpisode.podcast.*"])
-        .getMany()) as PodcastEpisodeNotesRecord[];
+  const { records } = await xata.db.PodcastEpisodeNotes.sort(
+    "createdAt",
+    "desc",
+  )
+    .select(["*", "podcastEpisode.*", "podcastEpisode.podcast.*"])
+    .getPaginated();
 
-      return { podcastNotes };
-    },
-    {
-      initialValue: { podcastNotes: [] },
-    },
-  );
-}
+  return records as PodcastEpisodeNotesRecord[];
+});
 
 export default function Home() {
-  const feed = useRouteData<typeof routeData>();
-
-  const { podcastNotes } = feed() ?? { notes: [], podcastNotes: [] };
-
-  // TODO: loading skeletons
+  const [data] = createResource(fetchPodcastNotes);
 
   return (
     <>
       <h1 class="font-bold text-xl text-center">Feed</h1>
 
-      <For each={podcastNotes}>
-        {(podcastNote) => <PodcastNote podcastNote={podcastNote} />}
-      </For>
+      <Suspense
+        fallback={
+          <div>
+            <PodcastNoteSkeleton />
+            <PodcastNoteSkeleton />
+            <PodcastNoteSkeleton />
+          </div>
+        }
+      >
+        <For each={data()}>
+          {(podcastNote) => <PodcastNote podcastNote={podcastNote} />}
+        </For>
+      </Suspense>
     </>
   );
 }
