d28e646e60a14cdcfc73907b559c623bb15e7c18
Andriy Pashynnyk <tobirawork@gmail.com>
2023-09-18 23:31:00 +0300

    add copy to clipboard with tooltip

--- src/components/PodcastNote.tsx
+++ src/components/PodcastNote.tsx
@@ -3,11 +3,20 @@ import { Datetime } from "~/components/Datetime";
 import { PodcastImageWithTitle } from "~/components/PodcastImageWithTitle";
 import { PodcastEpisodeTitle } from "~/components/PodcastEpisodeTitle";
 
+import tippy from "tippy.js";
+import "./tippy-teme.css";
+
 export function PodcastNote({
   podcastNote,
 }: {
   podcastNote: PodcastEpisodeNotesRecord;
 }) {
+  const text = podcastNote?.text ?? "";
+
+  function copyToClipboard() {
+    return navigator.clipboard.writeText(text);
+  }
+
   return (
     <div class="my-16 pl-4 border-l-2 border-gray-400 flex flex-col gap-4">
       <PodcastEpisodeTitle
@@ -21,7 +30,24 @@ export function PodcastNote({
         size="xs"
       />
 
-      <span class="py-6">{podcastNote.text}</span>
+      <div class="py-6">
+        <span
+          onClick={copyToClipboard}
+          ref={(el) => {
+            tippy(el, {
+              content: "Copied to clipboard!",
+              trigger: "click",
+              placement: "bottom",
+              arrow: true,
+              animation: "scale",
+              theme: "custom",
+              delay: [0, 1000],
+            });
+          }}
+        >
+          {text}
+        </span>
+      </div>
 
       <Datetime date={podcastNote.createdAt} />
     </div>
