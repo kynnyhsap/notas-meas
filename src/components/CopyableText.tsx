5e7f3ffe373a9c363a1a70389c734caa8e323e7e
Andriy Pashynnyk <tobirawork@gmail.com>
2023-09-19 13:54:45 +0300

    use tippy programmatically

---
+++ src/components/CopyableText.tsx
@@ -0,0 +1,37 @@
+import tippy, { Instance } from "tippy.js";
+import "tippy.js/dist/tippy.css";
+import "tippy.js/animations/scale.css";
+import "./tippy.theme.css";
+
+export function CopyableText({ children }: { children: string }) {
+  let tooltip: Instance;
+
+  async function copyToClipboard() {
+    await navigator.clipboard.writeText(children);
+
+    tooltip.show();
+
+    const timeoutId = setTimeout(() => {
+      tooltip.hide();
+      clearTimeout(timeoutId);
+    }, 2000);
+  }
+
+  return (
+    <span
+      onClick={copyToClipboard}
+      ref={(el) => {
+        tooltip = tippy(el, {
+          content: "Copied to clipboard",
+          animation: "scale",
+          placement: "bottom",
+          trigger: "manual",
+          theme: "custom",
+          duration: 300,
+        });
+      }}
+    >
+      {children}
+    </span>
+  );
+}
--- src/components/PodcastNote.tsx
+++ src/components/PodcastNote.tsx
@@ -3,21 +3,13 @@ import { Datetime } from "~/components/Datetime";
 import { PodcastImageWithTitle } from "~/components/PodcastImageWithTitle";
 import { PodcastEpisodeTitle } from "~/components/PodcastEpisodeTitle";
 
-import tippy from "tippy.js";
-import "tippy.js/dist/tippy.css";
-import "./tippy.theme.css";
+import { CopyableText } from "~/components/CopyableText";
 
 export function PodcastNote({
   podcastNote,
 }: {
   podcastNote: PodcastEpisodeNotesRecord;
 }) {
-  const text = podcastNote?.text ?? "";
-
-  async function copyToClipboard() {
-    await navigator.clipboard.writeText(text);
-  }
-
   return (
     <div class="my-16 pl-4 border-l-2 border-gray-400 flex flex-col gap-4">
       <PodcastEpisodeTitle
@@ -32,21 +24,7 @@ export function PodcastNote({
       />
 
       <div class="py-6">
-        <span
-          onClick={copyToClipboard}
-          ref={(el) => {
-            tippy(el, {
-              content: "Copied to clipboard",
-              delay: [200, 1000],
-              animation: "scale",
-              placement: "bottom",
-              trigger: "click",
-              theme: "custom",
-            });
-          }}
-        >
-          {text}
-        </span>
+        <CopyableText>{podcastNote?.text ?? ""}</CopyableText>
       </div>
 
       <Datetime date={podcastNote.createdAt} />
