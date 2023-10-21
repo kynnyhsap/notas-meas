1c257484cee204cac8052778da12412714d6e763
Andriy Pashynnyk <tobirawork@gmail.com>
2023-10-22 00:31:54 +0300

    fix

--- src/components/Opaque.tsx
+++ src/components/Opaque.tsx
@@ -1,5 +1,3 @@
-export function Opaque(props: { children: any; opacity?: number }) {
-  const opacity = () => props.opacity ?? 30;
-
-  return <div class={`opacity-${opacity()}`}>{props.children}</div>;
+export function Opaque(props: { children: any }) {
+  return <div class="opacity-30">{props.children}</div>;
 }
